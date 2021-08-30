ffea4daf9665ba1748c73269478a7fcf60849ccc
poprostu-armani <85347925+poprostu-armani@users.noreply.github.com>
2021-08-30 09:39:46 +0200

    home

--- js/script.js
+++ js/script.js
@@ -51,11 +51,17 @@ let tabBody = tabs.querySelector(".tabs__body");
 let tabIndicator = tabs.querySelector(".tabs__indicator");
 let tabHeaderNodes = tabs.querySelectorAll(".menu__link");
 let tabBodyNodes = tabs.querySelectorAll(".body__section");
+let worksButton = tabs.querySelector(".portfolio__btn");
+let worksSection = tabs.querySelector(".games");
+let worksLink = tabs.querySelector(".menu__works");
+let home = tabs.querySelector(".menu__home");
 
 for (let i = 0; i < tabHeaderNodes.length; i++) {
   tabHeaderNodes[i].addEventListener("click", function () {
     if (menuList.classList.contains("_show")) {
       menuList.classList.toggle("_show");
+      icon.classList.toggle("fa-bars");
+      icon.classList.toggle("fa-times");
     }
     tabHeader.querySelector("._active").classList.remove("_active");
     tabHeaderNodes[i].classList.add("_active");
@@ -65,6 +71,21 @@ for (let i = 0; i < tabHeaderNodes.length; i++) {
     e.preventDefault(e);
   });
 }
+
+worksButton.addEventListener("click", function (e) {
+  if (menuList.classList.contains("_show")) {
+    menuList.classList.toggle("_show");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-times");
+  }
+
+  tabHeader.querySelector("._active").classList.remove("_active");
+  worksLink.classList.add("_active");
+  tabBody.querySelector("._active").classList.remove("_active");
+  worksSection.classList.add("_active");
+  tabIndicator.style.left = `calc(calc(25%) )`;
+  e.preventDefault(e);
+});
 
 //SLIDER
 const wrapper = document.querySelector(".scroll__wrapper");
@@ -170,4 +191,211 @@ function prevBtn() {
   }
 }
 
-//BACKGROUND
+//HOME
+/**
+ * ITEManimate object is used to animate ease with bezier functions
+ * example: TweenMax.to($('selector'), 1.5, {left:"80%", ease: ITEManimate.bezier(0.04,0.86,0.8,1)});
+ *
+ * Used: https://github.com/rdallasgray/bez
+   Forked from: https://codepen.io/karlovidek/pen/qOxYjp
+ */
+var ITEManimate = {
+  start: 0,
+  bezier: function (p0, p1, p2, p3) {
+    return ITEManimate.polyBez([p0, p1], [p2, p3]);
+  },
+  polyBez: function (p1, p2) {
+    var A = [null, null],
+      B = [null, null],
+      C = [null, null],
+      bezCoOrd = function (t, ax) {
+        (C[ax] = 3 * p1[ax]),
+          (B[ax] = 3 * (p2[ax] - p1[ax]) - C[ax]),
+          (A[ax] = 1 - C[ax] - B[ax]);
+        return t * (C[ax] + t * (B[ax] + t * A[ax]));
+      },
+      xDeriv = function (t) {
+        return C[0] + t * (2 * B[0] + 3 * A[0] * t);
+      },
+      xForT = function (t) {
+        var x = t,
+          i = 0,
+          z;
+        while (++i < 14) {
+          z = bezCoOrd(x, 0) - t;
+          if (Math.abs(z) < 1e-3) break;
+          x -= z / xDeriv(x);
+        }
+        return x;
+      };
+    return function (t) {
+      return bezCoOrd(xForT(t), 1);
+    };
+  },
+};
+
+//CUSTOM JS CODE
+(function ($) {
+  //VIEWPORT
+  var w = $(window);
+
+  //ANIMATION
+  var animationTrigger1 = $(".trigger1");
+  var animationTrigger2 = $(".trigger2");
+  var sceneContainer = $(".slide-wrapper");
+  var smallCircles = $(".small-circle");
+  var portfolioContainer = $(".portfolio-wrapper");
+
+  var main = {
+    init: function () {
+      var self = this;
+      //GSAP ANIMATE
+      main.animate();
+    },
+
+    //GSAP ANIMATION
+    animate: function () {
+      //OPEN
+      function openAnimation() {
+        TweenMax.to(sceneContainer, 0.8, {
+          height: "100%",
+          ease: ITEManimate.bezier(0.93, 0.035, 0.35, 0.815),
+          top: "0%",
+          ease: ITEManimate.bezier(0.93, 0.035, 0.35, 0.815),
+          width: "100%",
+          ease: ITEManimate.bezier(0.93, 0.035, 0.35, 0.815),
+          left: "0%",
+          ease: ITEManimate.bezier(0.93, 0.035, 0.35, 0.815),
+
+          onComplete: function () {
+            console.log(sceneContainer);
+            TweenMax.to(portfolioContainer, 1.8, {
+              width: "100%",
+              ease: ITEManimate.bezier(0.93, 0.035, 0.35, 0.815),
+              top: "30%",
+              ease: ITEManimate.bezier(0.93, 0.035, 0.35, 0.815),
+            });
+            TweenMax.to(sceneContainer, 0.8, {
+              top: "-80%",
+              ease: ITEManimate.bezier(0.93, 0.035, 0.35, 0.815),
+            });
+          },
+        });
+
+        TweenMax.to(smallCircles, 0.4, {
+          scale: "0",
+          ease: ITEManimate.bezier(0.93, 0.035, 0.35, 0.815),
+        });
+      }
+
+      //CLOSE
+      function closeAnimation() {
+        TweenMax.to(portfolioContainer, 1.2, {
+          top: "100%",
+          ease: ITEManimate.bezier(0.815, 0.035, 0.35, 0.93),
+          width: "100%",
+          ease: ITEManimate.bezier(0.815, 0.035, 0.35, 0.93),
+
+          onComplete: function () {
+            TweenMax.to(sceneContainer, 0.8, {
+              height: "80%",
+              ease: ITEManimate.bezier(0.815, 0.035, 0.35, 0.93),
+              top: "10%",
+              ease: ITEManimate.bezier(0.815, 0.035, 0.35, 0.93),
+              width: "80%",
+              ease: ITEManimate.bezier(0.815, 0.035, 0.35, 0.93),
+              left: "10%",
+              ease: ITEManimate.bezier(0.815, 0.035, 0.35, 0.93),
+              onComplete: function () {
+                TweenMax.to(smallCircles, 0.4, {
+                  scale: "1",
+                  ease: ITEManimate.bezier(0.815, 0.035, 0.35, 0.93),
+                });
+              },
+            });
+          },
+        });
+      }
+
+      animationTrigger1.click(function () {
+        if ($(this).attr("data-toggle") == "closed") {
+          $(this).attr("data-toggle", "opened");
+          openAnimation();
+        } else if ($(this).attr("data-toggle", "opened")) {
+          $(this).attr("data-toggle", "closed");
+          closeAnimation();
+        }
+        if (animationTrigger2.attr("data-toggle") == "closed") {
+          animationTrigger2.attr("data-toggle", "opened");
+          openAnimation();
+        } else if (animationTrigger2.attr("data-toggle", "opened")) {
+          animationTrigger2.attr("data-toggle", "closed");
+          closeAnimation();
+        }
+      });
+
+      animationTrigger2.click(function () {
+        if ($(this).attr("data-toggle") == "closed") {
+          $(this).attr("data-toggle", "opened");
+          openAnimation();
+        } else if ($(this).attr("data-toggle", "opened")) {
+          $(this).attr("data-toggle", "closed");
+          closeAnimation();
+        }
+        if (animationTrigger1.attr("data-toggle") == "closed") {
+          animationTrigger1.attr("data-toggle", "opened");
+          openAnimation();
+        } else if (animationTrigger1.attr("data-toggle", "opened")) {
+          animationTrigger1.attr("data-toggle", "closed");
+          closeAnimation();
+        }
+      });
+    },
+  };
+
+  $(window).resize(function () {});
+  return main.init();
+})($);
+
+//TEXT ANIMATION
+
+$(document).ready(function () {
+  $(".main-home__title").lettering();
+});
+
+$(document).ready(function () {
+  animation();
+}, 300);
+
+$(".button").click(function () {
+  animation();
+});
+
+function animation() {
+  var title1 = new TimelineMax();
+  title1.to(".button", 0, { visibility: "hidden", opacity: 0 });
+  title1.staggerFromTo(
+    ".main-home__title",
+    0.5,
+    { ease: Back.easeOut.config(1.7), opacity: 0, bottom: -120 },
+    { ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0 },
+    0.05
+  );
+
+  title1.staggerFromTo(
+    ".main-home__subtitle",
+    0.5,
+    { ease: Back.easeOut.config(1.7), opacity: 0, bottom: -120 },
+    { ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0 },
+    0.05
+  );
+
+  title1.staggerFromTo(
+    ".main-home__btn",
+    0.3,
+    { ease: Back.easeOut.config(1.7), opacity: 0, bottom: -250 },
+    { ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0 },
+    0.05
+  );
+  title1.to(".button", 0.2, { visibility: "visible", opacity: 1 });
+}
